5ddfbb765aa05a8b1439726897ce605728105764
Austin Rodgers <austin.rodgers@aon.com>
2019-08-09 11:23:26 -0400

    Added NotMappepd DataAnnotation to Satellites.

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace StarChart.Models
 {
@@ -13,6 +11,7 @@ namespace StarChart.Models
         [Required]
         public string Name { get; set; }
         public int? OrbitedObjectId { get; set; }
+        [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
     }
